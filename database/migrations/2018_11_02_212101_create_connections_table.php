4b3ceb736ab4b5e30cc3d435fbb9d631ba1c39a5
Behrouz Khezry <bkhezry@gmail.com>
2018-11-03 01:10:55 +0330

    create connection table with model of it

---
+++ database/migrations/2018_11_02_212101_create_connections_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestsEmailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('connections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_uuid_from');
+            $table->string('user_uuid_to');
+            $table->string('learn_skill_uuid_from');
+            $table->string('teach_skill_uuid_from');
+            $table->string('email_from')->nullable();
+            $table->string('email_to')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('connections', function (Blueprint $table) {
+            //
+        });
+    }
+}
