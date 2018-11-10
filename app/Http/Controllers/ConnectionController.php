4528fdac1ceb7b326a80d418bf6b433f590bfbeb
Behrouz Khezry <bkhezry@gmail.com>
2018-11-11 00:44:12 +0330

    fixed bug in returning data from getConnection function.

--- app/Http/Controllers/ConnectionController.php
+++ app/Http/Controllers/ConnectionController.php
@@ -39,7 +39,7 @@ class ConnectionController extends Controller
     public function getConnection($uuid)
     {
         $connection_send = Connection::where('user_uuid_from', $uuid)->get();
-        $connection_receive = Connection::where('user_uuid_from', $uuid)->get();
+        $connection_receive = Connection::where('user_uuid_to', $uuid)->get();
         $connections = array();
         $connections['connection_send'] = $connection_send;
         $connections['connection_receive'] = $connection_receive;
