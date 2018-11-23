360bca98f09abf0f2c59470d0074080558e93ebf
Behrouz Khezry <bkhezry@gmail.com>
2018-11-24 02:07:17 +0330

    create abouts table migration

---
+++ database/migrations/2018_11_23_223142_create_abouts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAboutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('app_version');
+            $table->string('app_url');
+            $table->string('changelog_url');
+            $table->string('license_url');
+            $table->string('sponsor_name');
+            $table->string('sponsor_description');
+            $table->string('sponsor_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+}
