d4db996a2e2f92b1e912549af1bd26396693dae0
Behrouz Khezry <bkhezry@gmail.com>
2018-11-24 14:26:41 +0330

    add store function/not complete

--- app/About.php
+++ app/About.php
@@ -6,4 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class About extends Model
 {
+    protected $fillable = [
+        'app_version',
+        'app_url',
+        'changelog_url',
+        'license_url',
+        'sponsor_name',
+        'sponsor_description',
+        'sponsor_url'
+    ];
 }
