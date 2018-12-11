de5f4d64b8b11c31809b9b9008306ee1a20b11c8
Mehdi Alipour <mehdialipour1991@gmail.com>
2018-12-11 12:53:24 +0330

    foreign key constraints added

--- database/migrations/2018_12_11_090122_create_skills_table.php
+++ database/migrations/2018_12_11_090122_create_skills_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreateSkillsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,17 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('skills', function (Blueprint $table) {
             $table->increments('id');
             $table->string('uuid')->nullable();
-            $table->string('fa_name');
-            $table->string('en_name');
+            $table->unsignedInteger('category_id')->nullable();
+            $table->string("fa_name");
+            $table->string("en_name");
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
         });
     }
 
@@ -29,6 +34,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('skills');
     }
 }
