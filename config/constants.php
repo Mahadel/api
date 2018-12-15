ff80b2e8417fac58fe1a78ad5cee1fe3e142c84c
Behrouz Khezry <bkhezry@gmail.com>
2018-12-15 18:12:25 +0330

    add constants.php file that provide google client_id and rsa keys for jwt auth

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    //https://developers.google.com/identity/sign-in/android/backend-auth
+    'client_id' => '',
+    'public_key' =>'',
+    'private_key' =>''
+    //http://travistidwell.com/jsencrypt/demo/
+];
